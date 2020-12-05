b672a447ad49f814483af3bfc2fa087fd3d5fb39
Suraj Srinivasa <suraj.srinivasa@oracle.com>
2020-12-04 22:20:02 -0500

    Day3: Toboggan trajectory
    Part1: Calculate the number of trees encountered when traversing from the top to the bottom with the specified slope of 3 right , 1 down
    Part2: Calculate the product of the number of trees encountered for different slope values - (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)

---
+++ day3_toboggan_trajectory/toboggan_trajectory.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+        return
+	}
+	strData := strings.Split(string(data), "\n")
+	right := 3
+	down := 1
+	fmt.Println(part1(strData, right, down))
+	fmt.Println(part2(strData))
+}
+
+func part1(strData []string, right int, down int) int {
+	rowLen := len(strData[0])
+	rowNum := 0
+	colNum := 0
+
+	treeCount := 0
+
+	for rowNum < len(strData) && len(strData[rowNum]) > 0 {
+		if colNum >= rowLen - 1 {
+			colNum = colNum % rowLen
+		}
+		if string(strData[rowNum][colNum]) == string('#') {
+			treeCount++
+		}
+		rowNum += down
+		colNum += right
+	}
+	
+	return treeCount
+}
+
+func part2(strData []string) int {
+	product := 1
+	product *= part1(strData, 1, 1)
+	product *= part1(strData, 3, 1)
+	product *= part1(strData, 5, 1)
+	product *= part1(strData, 7, 1)
+	product *= part1(strData, 1, 2)
+	return product
+}
